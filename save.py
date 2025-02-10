0a7f273c71fa42952493e8e32d23e276088d5ae0
hectordll <contactdealh@gmail.com>
2025-02-10 17:09:32 +0100

    Add catfish sales data and implement ARIMA model for sales prediction

---
+++ save.py
@@ -0,0 +1,59 @@
+log_catfish_sales = numpy.log(catfish_sales)
+
+# création d'un graphique avec la série originale
+g, ax = plt.subplots()
+ln1 = ax.plot(catfish_sales, c='r', label='Ventes de poissons-chats (1000s de livres)')
+
+# création d'un graphique avec la série log en conservent le même axe des abscisses
+ax2 = ax.twinx()
+ln2 = ax2.plot(log_catfish_sales, c='b', label='Log(Ventes de poissons-chats)')
+
+# ajout de la légende
+lns = ln1 + ln2
+labels=[l.get_label() for l in lns]
+ax.legend(lns, labels)
+
+# Séparer les données en ensemble d'entraînement et ensemble de test
+train_data = catfish_sales[:-15]
+test_data = catfish_sales[-15:]
+
+# Utiliser auto_arima pour trouver le meilleur modèle ARIMA
+# model = pm.auto_arima(train_data)
+model = pm.auto_arima(train_data, seasonal=False, stepwise=True, suppress_warnings=True)
+ 
+
+print(model.summary())
+
+# Ajuster le modèle aux données
+model.fit(train_data)
+# Obtenir les résidus du modèle
+residuals = model.resid()
+plot_acf_pacf({
+    'Residue modèle': residuals
+})
+
+# Faire des prédictions sur l'ensemble d'entraînement
+train_pred, train_confint = model.predict_in_sample(return_conf_int=True)
+
+# Faire des prédictions sur l'ensemble de test
+n_periods = len(test_data)
+predicted, confint = model.predict(n_periods=n_periods, return_conf_int=True)
+
+# Concaténer les prédictions pour l'ensemble d'entraînement et de test
+all_predictions = pandas.concat([pandas.Series(train_pred, index=train_data.index),
+                            pandas.Series(predicted, index=test_data.index)],
+                            axis=0)
+
+# Tracer les valeurs réelles et les prédictions pour l'ensemble d'entraînement et de test
+plt.figure(figsize=(12, 6))
+plt.plot(train_data, label='Ventes observées (Entraînement)', color='blue')
+plt.plot(test_data, label='Ventes observées (Test)', color='green')
+plt.plot(all_predictions, label='Ventes prédites', color='red')
+
+plt.xlabel('Date')
+plt.ylabel('Ventes de poissons-chats (1000s de livres)')
+plt.title('Ventes observées vs prédites de poissons-chats')
+plt.legend()
+plt.grid(True)
+# plt.savefig("prediction.png")
+plt.show()
